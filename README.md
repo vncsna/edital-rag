2f1a6377cf8e4bd5f0516c3f5d8acfc5454ede14
Vinicius Aguiar <vncsna@gmail.com>
2024-08-10 18:43:00 -0300

    feat: update langchain chatbot to support chat context

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-## edital-rag
+## Edital RAG
 
-RAG para o edital do vestibular da Unicamp em 2025
+- ChatBot sobre o vestibular da Unicamp de 2025
 
 
 ## References
 
 - https://python.langchain.com/v0.2/docs/tutorials/rag/
+- https://python.langchain.com/v0.2/docs/how_to/chatbots_retrieval/
 - https://medium.com/@prasadmahamulkar/introduction-to-retrieval-augmented-generation-rag-using-langchain-and-lamaindex-bd0047628e2a
--- app/app/main.py
+++ app/app/main.py
@@ -1,15 +1,17 @@
 from os import getenv
 from pathlib import Path
+from textwrap import dedent
 
 import streamlit as st
-from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.retrievers import ParentDocumentRetriever
 from langchain.storage import InMemoryStore
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import TextLoader
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnableBranch, RunnablePassthrough
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 from pypdf import PdfReader
@@ -72,28 +74,62 @@ def generate_chain():
     )
     retriever.add_documents(documents)
 
-    llm = ChatOpenAI(model_name=OPENAI_MODEL_NAME)
+    chat = ChatOpenAI(model_name=OPENAI_MODEL_NAME)
 
-    template = """
-    Voce e um assistente de atendimento que conversa sobre
-    o edital do vestibular da unicamp em 2025 com jovens e adultos.
-    Use apenas dados contidos no contexto para responder as questoes a seguir.
+    # Summarize chat into a question to retrieve context
 
-    <context>
-    {context}
-    </context>
+    query_transform_prompt = ChatPromptTemplate.from_messages(
+        [
+            MessagesPlaceholder(variable_name="messages"),
+            (
+                "user",
+                "Dada a conversa acima, gere uma pergunta que deve obter "
+                "informações relevantes para a conversa. Responda apenas com a pergunta.",
+            ),
+        ]
+    )
+    query_transforming_retriever_chain = RunnableBranch(
+        (
+            # Condition to check
+            lambda x: len(x.get("messages", [])) == 1,
+            # If only one message, then we just pass that message's content to retriever
+            (lambda x: x["messages"][-1]["content"]) | retriever,
+        ),
+        # Else, then we pass inputs to LLM chain to transform the query, then pass to retriever
+        query_transform_prompt | chat | StrOutputParser() | retriever,
+    ).with_config(run_name="chat_retriever_chain")
 
-    Questao: {input}
-    """
-    prompt = ChatPromptTemplate.from_template(template)
+    # Use the system template to answer the question
 
-    documents_chain = create_stuff_documents_chain(llm, prompt)
-    retrieval_chain = create_retrieval_chain(retriever, documents_chain)
+    template = dedent("""
+        Voce e um assistente de atendimento que conversa sobre
+        o edital do vestibular da unicamp em 2025 com jovens e adultos.
+        Use apenas dados contidos no contexto para responder as questoes a seguir.
 
-    return retrieval_chain
+        <context>
+        {context}
+        </context>
+    """)
+    question_answering_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", template),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
+    )
+    document_chain = create_stuff_documents_chain(chat, question_answering_prompt)
+
+    # Answer the last question with the chat and docs context
+
+    conversational_retrieval_chain = (
+        RunnablePassthrough
+        .assign(context=query_transforming_retriever_chain)
+        .assign(answer=document_chain)
+    )  # fmt: skip
+
+    return conversational_retrieval_chain, retriever
 
 
-chain = generate_chain()
+chain, retriever = generate_chain()
 
 
 ########################################
@@ -117,8 +153,9 @@ with tab1:
         # Load user input, process and return assistant output
         if prompt := st.chat_input("Digite uma dúvida"):
             st.session_state.messages.append({"role": "user", "content": prompt})
-            response = chain.invoke({"input": prompt})
+            response = chain.invoke({"messages": st.session_state.messages})
             st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
+            print(f"DEBUG: {response}")
             st.rerun()
 
 
